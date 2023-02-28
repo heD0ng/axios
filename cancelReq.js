f7f9bdcd294a056b93d7933a778199bd3ac9b67a
hedong <13545403924@163.com>
2023-02-28 21:53:39 +0800

    feat:兼容cancel.token

--- cancelReq.js
+++ cancelReq.js
@@ -6,16 +6,27 @@ export default class CancelReq {
         this.keyMap = new Map();
     }
     genKey(config) {
-        const {url, method, data = {}, params = {}} = config;
+        const {
+            url,
+            method,
+            data = {},
+            params = {}
+        } = config;
         return [url, method, data, params].map(item => qs.stringify(item)).join('-');
     }
     addKey(config, CancelToken) {
         const key = this.genKey(config);
-        config.cancelToken = config.cancelToken || new CancelToken((c) => {
-            if(!this.keyMap.has(key)) {
-                this.keyMap.set(key, c);
+        if (config.cancelToken) {
+            if (!this.keyMap.has(key)) {
+                this.keyMap.set(key, config.cancelToken);
             }
-        })
+        } else {
+            config.cancelToken = new CancelToken((c) => {
+                if (!this.keyMap.has(key)) {
+                    this.keyMap.set(key, c);
+                }
+            })
+        }
     }
     // 判断存在并取消
     cancel(config) {
